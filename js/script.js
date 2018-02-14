666a1671833f77c7606062b323de2d620b8df8a6
Ashley Diaz <ashleyd0648@tywls.org>
2018-02-14 23:27:59 +0000

    added for loop myPlayList

--- js/script.js
+++ js/script.js
@@ -27,22 +27,23 @@ var mySong = {
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"title":"Across the Universe",
+		"artist":"The Beatles",
+		"mp3-url":"https://open.spotify.com/track/4dkoqJrP0L8FXftrMZongF",
+		"image-url":"https://i.scdn.co/image/b3651a85f2bca826b38194c51d09cd7b068aa3ab",
+
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title":"The Party's Crashing Us ",
+		"artist":"Of Montreal",
+		"mp3-url":"https://open.spotify.com/track/12zvitvNTaAM6HxMPGm1vh",
+		"image-url":"https://i.scdn.co/image/029db5b208be1718359eb000e9ffca87ca035157",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title":"Under Cover of Darkness",
+		"artist":"The Strokes",
+		"mp3-url":"https://open.spotify.com/track/6u0x5ad9ewHvs3z6u9Oe3c",
+		"image-url":"https://i.scdn.co/image/c1b2df9385e01aedc5288dfa315329f4c1dd79e1",
 	}
 
 ]
@@ -59,6 +60,15 @@ $( document ).ready(function() {
 
 
 
+
+
+		for(var i = 0; i < myPlayList.length; i++){
+			
+			$("body").append("<p> Title:" + myPlayList[i].title + "</p>");
+    		$("body").append("<p> Artist:" + myPlayList[i].artist + "</p>");
+			$("body").append("<a href ='" + myPlayList[i].mp3-url + "'> Play Song </a>");
+			$("body").append("<img src =" + myPlayList[i].image-url + ">");
+	
 });
 
 function displayList(){
